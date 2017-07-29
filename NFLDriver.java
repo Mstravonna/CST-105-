2c0166ec88623a61b35d08c077830263404af66f
Travonna Wilson <travonna@yahoo.com>
2017-07-29 01:04:52 -0500

    MileStone 2 NFL Driver Class revised (i hope this is correct because I got confused)

---
+++ NFLDriver.java
@@ -0,0 +1,82 @@
+//Travonna Wilson
+//7/29/17
+//CST-105
+//this is my own work
+
+
+package nfldriver;
+
+/**
+ *
+ * @author Travonna
+ */
+public class NFLDriver {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    NFLDriver() {
+		setName("");
+		setAge(0);
+		setNumber(0);
+		setTeam("");
+		setHeight(0); 
+		setWeight(0);
+		setCategory("");
+	}
+	
+	NFLDriver(String name, int age, int number, String team, float height, float weight, String category) {
+		this.setName(name);
+		this.setAge(age);
+		this.setNumber(number);
+		this.setTeam(team);
+		this.setHeight(height); 
+		this.setWeight(weight);
+		this.setCategory(category);
+	}
+
+        
+    private void setName(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void setAge(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void setNumber(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void setTeam(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void setHeight(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void setWeight(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void setCategory(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void setHeight(float height) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void setWeight(float weight) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+        }
+    
+
